ee8c51ef5603907834118dcb552cfd2be80de080
alexandreSouza31 <alexandre31_m@hotmail.com>
2023-05-04 14:41:55 -0300

    will create react folder and index.html file structure

---
+++ satisfaction_survey/src/App.jsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="app">
+      <header className="header">
+        <h2>Deixe sua avaliação</h2>
+        <p>Obrigado por comprar conosco! Preencha o formulário abaixo para avaliar o produto:</p>
+      </header>
+      <main>
+        <p>etapas</p>
+        <form>
+
+        </form>
+      </main>
+    </div>
+  )
+}
+
+export default App
