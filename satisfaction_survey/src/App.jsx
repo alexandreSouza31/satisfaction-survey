9ca5fd0bbbfa712805cf32a9aba18325e81f21dd
alexandreSouza31 <alexandre31_m@hotmail.com>
2023-05-05 22:46:07 -0300

    will adjust page styles

--- satisfaction_survey/src/App.jsx
+++ satisfaction_survey/src/App.jsx
@@ -5,6 +5,7 @@ import { FiSend } from "react-icons/fi"
 import UserForm from "./components/UserForm";
 import ReviewForm from "./components/ReviewForm";
 import Thanks from "./components/Thanks";
+import Steps from "./components/Steps";
 
 //hooks
 import { useForm } from "./hooks/useForm";
@@ -25,9 +26,9 @@ function App() {
         <h2>Deixe sua avaliação</h2>
         <p>Obrigado por comprar conosco! Preencha o formulário abaixo para avaliar o produto:</p>
       </header>
-      <main>
-        <p>etapas</p>
-        <form onSubmit={(e)=>changeStep(currentStep + 1,e)} className="form-container">
+      <main className="form-container">
+        <Steps currentStep={currentStep}/>
+        <form onSubmit={(e)=>changeStep(currentStep + 1,e)} className="form">
           <div className="inputs-container">{currentComponent}</div>
           <div className="actions">
             {!isFirstStep && (
