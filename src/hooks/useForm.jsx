4908358d0b99a317bb5a11c4a9df4a24bcd30900
alexandreSouza31 <alexandre31_m@hotmail.com>
2023-05-09 19:55:49 -0300

    will change the logic of the steps

--- src/hooks/useForm.jsx
+++ src/hooks/useForm.jsx
@@ -15,7 +15,8 @@ export function useForm(steps) {
         currentStep,
         currentComponent: steps[currentStep],
         changeStep,
-        isLastStep:currentStep+1===steps.length?true:false,
-        isFirstStep:currentStep=== 0?true:false,
+        isSecondStep: currentStep === 2?true:false ,
+        isFirstStep: currentStep === 0 ? true : false,
+        isThirdStep:currentStep+1===steps.length?true:false
     };
 }
